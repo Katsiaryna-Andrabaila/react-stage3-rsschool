a617d1f75af49b6b3e7b06ef10bb804159375ece
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-03-15 19:48:32 +0300

    feat: add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,6 +3,21 @@ import './Footer.css';
 
 export default class Footer extends React.Component {
   render() {
-    return <footer className="footer">Ghdbtn</footer>;
+    return (
+      <footer className="footer">
+        <a
+          href="https://github.com/Katsiaryna-Andrabaila"
+          target="_blank"
+          rel="noreferrer"
+          className="github-link"
+        >
+          katsiaryna-andrabaila
+        </a>
+        <p>2023</p>
+        <a href="https://rs.school/react/" target="_blank" rel="noreferrer" className="rss-link">
+          <img src="https://rs.school/images/rs_school_js.svg" />
+        </a>
+      </footer>
+    );
   }
 }
