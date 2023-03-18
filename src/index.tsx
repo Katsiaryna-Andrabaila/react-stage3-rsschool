f1bfa004a89c103eda0ed87588a8cf0c17be3668
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-03-18 11:55:34 +0300

    fix: rewrite router, fix linter errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import About from 'pages/about/About';
 import NotFound from 'pages/404/404';
 
@@ -10,7 +10,11 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <NotFound />,
+    errorElement: <Navigate to="/404" replace />,
+  },
+  {
+    path: '/404',
+    element: <NotFound />,
   },
   {
     path: 'about',
