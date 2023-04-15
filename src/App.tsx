f34b3023579a064367cec3b3b4e17012d6913dd6
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-04-15 22:51:54 +0300

    refactor: remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 import Main from './pages/main/Main';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import { Item } from './types/types';
-import { getItems } from './api/getItems';
-import { searchItems } from './api/searchItems';
-import { useDispatch } from 'react-redux';
 import { useAppSelector } from './redux/hooks';
 import { useGetItemsQuery } from './redux/reducers';
 
 const App = () => {
   const { search } = useAppSelector((state) => state.search);
   const { data: defaultCards } = useGetItemsQuery(search);
-
-  /* const [cards, setCards] = useState<Item[]>([]);
-
-  useEffect(() => {
-    const setDefaultCards = async () => {
-      // const searchValue = localStorage.getItem('search-key987');
-      const resultCards = search ? await searchItems(search) : await getItems();
-      setCards(resultCards);
-    };
-
-    setDefaultCards();
-  }, [search]); */
 
   return (
     <div className="App">
